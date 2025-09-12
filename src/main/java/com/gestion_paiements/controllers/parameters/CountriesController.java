07d73ca45ea086024006abfeeb4120367ff6e65a
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-09-12 10:54:08 +0300

    Progress on the parameters window, trying to make the size work

--- src/main/java/com/gestion_paiements/controllers/parameters/CountriesController.java
+++ src/main/java/com/gestion_paiements/controllers/parameters/CountriesController.java
@@ -2,6 +2,7 @@ package com.gestion_paiements.controllers.parameters;
 
 import com.gestion_paiements.types.SampleData;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -9,6 +10,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class CountriesController {
 
@@ -25,9 +27,13 @@ public class CountriesController {
     @FXML
     private TextArea commentSection;
 
+    // A mutable ObservableList to hold the countries for the ListView
+    private ObservableList<String> countryList;
+
     @FXML
     private void initialize() {
-        listViewCountries.setItems(FXCollections.observableList(SampleData.instance.getSetCountries().stream().sorted().toList()));
+        countryList = FXCollections.observableArrayList(SampleData.instance.getSetCountries().stream().sorted().collect(Collectors.toList()));
+        listViewCountries.setItems(countryList);
     }
 
     // Adds the country to the list if possible, otherwise shows a message
@@ -45,13 +51,12 @@ public class CountriesController {
 
         else {
             SampleData.instance.getSetCountries().add(input);
+            countryList.add(input);
+            FXCollections.sort(countryList);
             commentSection.setText("Pays ajouté");
-            // TODO update the listview to show the result
         }
 
         // Resets the input
         textFieldNewCountry.setText("");
     }
-
-
 }
