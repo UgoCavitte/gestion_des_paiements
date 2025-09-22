94585fee4a281447f2fdf7e1a1890a61a4761dc7
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-09-22 20:46:54 +0200

    Added a Set to String converter for PurchasedProduct elements

---
+++ src/main/java/com/gestion_paiements/util/PurchasedProducts.java
@@ -0,0 +1,18 @@
+package com.gestion_paiements.util;
+
+import com.gestion_paiements.types.PurchasedProduct;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+public abstract class PurchasedProducts {
+
+    static List<String> fromSetToStrings (Set<PurchasedProduct> set) {
+        return set.stream()
+                .sorted(Comparator.comparing(p -> p.getProduct().getId()))
+                .map(p -> p.getQuantity() + p.getProduct().getShortName()).toList();
+    }
+
+}
