0562cf2009595baf3bd1df5c8e24565f9ec58237
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-09-24 11:08:46 +0200

    Added the parameters for the columns to show in PlatformTableController

--- src/main/java/com/gestion_paiements/controllers/parameters/ColumnsToShowController.java
+++ src/main/java/com/gestion_paiements/controllers/parameters/ColumnsToShowController.java
@@ -28,6 +28,30 @@ public class ColumnsToShowController {
     @FXML
     private CheckBox BASender;
 
+    @FXML
+    private CheckBox PAmountReceived;
+
+    @FXML
+    private CheckBox PAmountSent;
+
+    @FXML
+    private CheckBox PDateReceived;
+
+    @FXML
+    private CheckBox PDateSent;
+
+    @FXML
+    private CheckBox PIDEnvoiCompte;
+
+    @FXML
+    private CheckBox PId;
+
+    @FXML
+    private CheckBox PProducts;
+
+    @FXML
+    private CheckBox PSender;
+
     public void initialize() {
         BAAmountReceived.setSelected(Preferences.ColumnsToShow.BAAmountReceived);
         BAAmountSent.setSelected(Preferences.ColumnsToShow.BAAmountSent);
@@ -77,6 +101,54 @@ public class ColumnsToShowController {
     @FXML
     void BASender() {
         Preferences.ColumnsToShow.BASender = BASender.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PAmountReceived() {
+        Preferences.ColumnsToShow.PAmountReceived = PAmountReceived.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PAmountSent() {
+        Preferences.ColumnsToShow.PAmountSent = PAmountSent.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PDateReceived() {
+        Preferences.ColumnsToShow.PDateReceived = PDateReceived.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PDateSent() {
+        Preferences.ColumnsToShow.PDateSent = PDateSent.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PId() {
+        Preferences.ColumnsToShow.PId = PId.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PProducts() {
+        Preferences.ColumnsToShow.PProducts = PProducts.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PSender() {
+        Preferences.ColumnsToShow.PSender = PSender.isSelected();
+        RefreshableData.refreshTables();
+    }
+
+    @FXML
+    void PIDEnvoiCompte() {
+        Preferences.ColumnsToShow.PSentToBank = PIDEnvoiCompte.isSelected();
         RefreshableData.refreshTables();
     }
 
