3382295851cc1aacf680c01004d8285ef9253d55
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-09-27 10:43:36 +0200

    Fixed window size when not resized

--- src/main/java/com/gestion_paiements/Main.java
+++ src/main/java/com/gestion_paiements/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 960);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Gestion des paiements");
         stage.setScene(scene);
         stage.show();
