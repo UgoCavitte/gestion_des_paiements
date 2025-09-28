8a7183b895b7c57b44695afb6d7edac09e045396
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-09-28 19:09:09 +0200

    Implemented the Comparable interface

--- src/main/java/com/gestion_paiements/types/Country.java
+++ src/main/java/com/gestion_paiements/types/Country.java
@@ -2,10 +2,11 @@ package com.gestion_paiements.types;
 
 import com.gestion_paiements.util.IDs;
 import com.gestion_paiements.util.WithID;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashSet;
 
-public class Country implements WithID {
+public class Country implements WithID, Comparable<Country> {
 
     private int id;
 
@@ -36,5 +37,10 @@ public class Country implements WithID {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int compareTo(@NotNull Country country) {
+        return this.name.compareTo(country.getName());
     }
 }
