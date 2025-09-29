48ccb182336ca529973790b2c401bc93a6e1f497
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-09-29 15:43:12 +0200

    Made the table sortable

--- src/main/java/com/gestion_paiements/controllers/TableClientsController.java
+++ src/main/java/com/gestion_paiements/controllers/TableClientsController.java
@@ -8,6 +8,8 @@ import com.gestion_paiements.util.Refreshable;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -71,8 +73,14 @@ public class TableClientsController implements Refreshable {
 
         });
 
-        List<Client> clients = Data.instance.getSetClients().stream().toList();
-        tableClients.setItems(FXCollections.observableList(clients));
+        // List<Client> clients = Data.instance.getSetClients().stream().toList();
+        // tableClients.setItems(FXCollections.observableList(clients));
+
+        ObservableList<Client> masterData = FXCollections.observableArrayList(Data.instance.getSetClients().stream().toList());
+        SortedList<Client> sortedData = new SortedList<>(masterData);
+        sortedData.comparatorProperty().bind(tableClients.comparatorProperty());
+        tableClients.setItems(sortedData);
+
     }
 
     @FXML
@@ -130,8 +138,8 @@ public class TableClientsController implements Refreshable {
 
     @Override
     public void refreshElement() {
-        tableClients.setItems(FXCollections.observableList(Data.instance.getSetClients().stream().toList()));
-        tableClients.refresh();
-        System.out.println("Refreshing");
+        ObservableList<Client> masterData = (ObservableList<Client>) ((SortedList<Client>) tableClients.getItems()).getSource();
+        masterData.clear();
+        masterData.addAll(Data.instance.getSetClients().stream().toList());
     }
 }
