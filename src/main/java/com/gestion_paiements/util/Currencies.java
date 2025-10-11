0671e9ddd63091180de0ef6bbbf1e2555d9c0670
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-10-11 11:00:01 +0200

    Added the "fromStringToCurrency" method

--- src/main/java/com/gestion_paiements/util/Currencies.java
+++ src/main/java/com/gestion_paiements/util/Currencies.java
@@ -1,12 +1,20 @@
 package com.gestion_paiements.util;
 
 import com.gestion_paiements.types.Amount;
+import com.gestion_paiements.types.Currency;
+import com.gestion_paiements.types.Data;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public abstract class Currencies {
 
     public static @NotNull String fromAmountToString (@NotNull Amount amount) {
         return amount.getAmount() + " " + amount.getCurrency();
+    }
+
+    public static Currency fromStringToCurrency(String name) {
+        return Data.instance.getSetCurrencies().stream().filter(c -> Objects.equals(c.getName(), name)).findFirst().orElse(null);
     }
 
 }
