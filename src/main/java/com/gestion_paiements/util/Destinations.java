79ba61db949fde5d3fdbcda970bd2f4615ac03c8
Ugo Cavitte <contact.ugo.francuz@gmail.com>
2025-12-17 16:28:26 +0100

    Redid the search algorithm faster

--- src/main/java/com/gestion_paiements/util/Destinations.java
+++ src/main/java/com/gestion_paiements/util/Destinations.java
@@ -4,15 +4,22 @@ import com.gestion_paiements.types.Data;
 import com.gestion_paiements.types.Destination;
 
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Destinations {
 
     public static Destination fromStringToDestination(String string) {
-
+        /*
         return Data.instance.getMapAccountsCountries()
                 .values().stream()
                 .flatMap(wc -> wc.getAccountsAndPlatforms().values().stream())
-                .filter(destination -> Objects.equals(destination.getName(), string)).findFirst().orElse(null);
+                .filter(destination -> Objects.equals(destination.getName(), string)).findFirst().orElse(null);*/
+
+        Set<String> toPrint = Data.instance.getSetDestinations().stream().map(Destination::getName).collect(Collectors.toSet());
+        System.out.println(toPrint);
+
+        return Data.instance.getSetDestinations().stream().filter(d -> Objects.equals(d.getName(), string)).findFirst().orElse(null);
     }
 
 }
